c513f59b9ab45a3b1aa1e203ad3835e61c423176
atul-blok <atul@blokument.com>
2023-05-07 19:02:21 +0530

    added the new lines

--- index.js
+++ index.js
@@ -22,18 +22,43 @@ function Test() {
   console.log(
     result2 === expected2 ? "Test Case 3 Passed" : "Test Case 3 Failed"
   );
+
+  // Add method to handle an unknown amount of numbers
+  const numbers3 = "10,25";
+  const expected3 = 35;
+  const result3 = Add(numbers3);
+  console.log(
+    result3 === expected3 ? "Test Case 4 Passed" : "Test Case 4 Failed"
+  );
+
+  // Add method to handle new lines between numbers (instead of commas)
+  const numbers4 = "1\n2,3";
+  const expected4 = 6;
+  const result4 = Add(numbers4);
+  console.log(
+    result4 === expected4 ? "Test Case 5 Passed" : "Test Case 5 Failed"
+  );
+
+  // Add method to handle new lines between numbers (instead of commas)
+  const numbers5 = "1,\n";
+  const expected5 = "Input is NOT ok";
+  const result5 = Add(numbers5);
+  console.log(
+    result5 === expected5 ? "Test Case 6 Passed" : "Test Case 6 Failed"
+  );
 }
 
 // Function implementation
 function Add(numbers) {
   if (numbers === "") return 0;
 
-  let delimiters = [","];
+  let delimiters = [",", "\n"];
 
   const numberArray = numbers.split(new RegExp(delimiters.join("|")));
   let sum = 0;
 
   for (const num of numberArray) {
+    if (num == "") return "Input is NOT ok";
     const number = parseInt(num);
     sum += number;
   }
